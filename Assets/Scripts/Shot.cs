2c34818ae8d4ce21cbf1f8f4a8e9cc61873361db
Ryoga_Yamauchi <g1854003@cc.kyoto-su.ac.jp>
2019-08-07 16:58:57 +0900

    first commit

---
+++ Assets/Scripts/Shot.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shot : MonoBehaviour
+{
+    public Transform ShotPoint;
+
+    public GameObject BulletPrefab;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject bullet = Instantiate(BulletPrefab,ShotPoint.transform.position, Quaternion.identity);
+            bullet.GetComponent<Rigidbody>().AddForce(100000f * transform.forward);
+        }
+    }
+}
---
+++ Assets/Scripts/Top.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.IMGUI.Controls;
+using UnityEngine;
+
+public class Top
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
